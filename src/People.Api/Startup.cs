c68ea3e86a18465a69893d64a17b07b204475885
Nail Makhmutov <makhmutov.nail@gmail.com>
2021-03-09 08:57:54 +0300

    [#21] Added logging and validation behaviors

--- src/People.Api/Startup.cs
+++ src/People.Api/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Confluent.Kafka;
 using CorrelationId;
 using CorrelationId.DependencyInjection;
 using FluentValidation;
@@ -9,17 +10,26 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using People.Api.Application.Behaviors;
+using People.Api.Application.IntegrationEventHandlers;
 using People.Api.Grpc;
+using People.Api.Infrastructure.EmailBuilder;
 using People.Api.Infrastructure.Interceptors;
+using People.Api.Infrastructure.IpAddress;
 using People.Api.Infrastructure.Password;
 using People.Api.Infrastructure.Providers.Google;
 using People.Api.Infrastructure.Providers.Microsoft;
 using People.Domain.AggregateModels.Account;
+using People.Host;
 using People.Infrastructure;
 using People.Infrastructure.Confirmations;
+using People.Infrastructure.Countries;
 using People.Infrastructure.Forbidden;
+using People.Infrastructure.IntegrationEvents;
+using People.Infrastructure.Kafka;
+using People.Infrastructure.Mongo;
 using People.Infrastructure.Repositories;
 using People.Infrastructure.Sequences;
+using People.Infrastructure.Timezones;
 
 namespace People.Api
 {
@@ -40,9 +50,12 @@ namespace People.Api
                 })
                 .WithTraceIdentifierProvider();
 
-            services.Configure<DbContextSettings>(Configuration.GetSection("Mongodb"))
+            services
+                .Configure<DbContextSettings>(Configuration.GetSection("Mongodb"))
                 .AddScoped<PeopleDbContext>()
                 .AddScoped<InfrastructureDbContext>()
+                .AddScoped<ICountryService, CountryService>()
+                .AddScoped<ITimezoneService, TimezoneService>()
                 .AddScoped<IForbiddenService, ForbiddenService>()
                 .AddScoped<ISequenceGenerator, SequenceGenerator>()
                 .AddScoped<IAccountRepository, AccountRepository>();
@@ -50,10 +63,11 @@ namespace People.Api
             services
                 .Configure<PasswordValidationOptions>(Configuration.GetSection("PasswordValidation"))
                 .AddScoped<IPasswordValidator, PasswordValidator>()
-                .AddSingleton<IPasswordHasher>(_ => new PasswordHasher(Configuration["App:Key"]));
+                .AddSingleton<IPasswordHasher>(_ => new PasswordHasher(Configuration["App:Key"]))
+                .AddSingleton<IIpAddressHasher>(_=> new IpAddressHasher(Configuration["App:Key"]));
 
             services.AddScoped<IConfirmationService, ConfirmationService>();
-            
+
             services.AddHttpClient<IGoogleApiService, GoogleApiService>(client =>
                 client.BaseAddress = new Uri(Configuration["Urls:GoogleApi"])
             );
@@ -61,9 +75,33 @@ namespace People.Api
             services.AddHttpClient<IMicrosoftApiService, MicrosoftApiService>(client =>
                 client.BaseAddress = new Uri(Configuration["Urls:MicrosoftApi"])
             );
-            
+
+            services.AddKafkaMessageBus()
+                .ConfigureProducers(config => config.BootstrapServers = Configuration["Kafka:Servers"])
+                .ConfigureConsumers(config =>
+                {
+                    config.BootstrapServers = Configuration["Kafka:Servers"];
+
+                    config.GroupId = Program.AppName;
+                    config.AutoOffsetReset = AutoOffsetReset.Earliest;
+                    config.EnableAutoCommit = false;
+                    config.EnablePartitionEof = true;
+                    config.AllowAutoCreateTopics = true;
+                })
+                .AddProducer<EmailMessageCreatedIntegrationEvent>(
+                    config => config.Topic = IntegrationEvent.EmailMessages
+                )
+                .AddProducer<AccountCreatedIntegrationEvent>(config => config.Topic = IntegrationEvent.CreatedAccounts)
+                .AddConsumer<AccountInfoReceivedIntegrationEvent, AccountInfoEventHandler>(config =>
+                {
+                    config.Topic = IntegrationEvent.CollectedInformation;
+                    config.Threads = 2;
+                });
+
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            services.AddMediatR(assemblies)
+            services
+                .AddEmailBuilder()
+                .AddMediatR(assemblies)
                 .AddValidatorsFromAssemblies(assemblies)
                 .AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
                 .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
@@ -79,17 +117,14 @@ namespace People.Api
                 })
                 .UseCorrelationId()
                 .UseRouting()
+                .UsePeopleLocalization()
                 .UseEndpoints(endpoints =>
                 {
                     endpoints.MapGrpcService<GatewayService>();
                     endpoints.MapGrpcService<IdentityService>();
 
-                    endpoints.MapGet("/",
-                        async context =>
-                        {
-                            await context.Response.WriteAsync(
-                                "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                        });
+                    endpoints.MapGet("/", context => context.Response
+                        .WriteAsync("Communication with gRPC endpoints must be made through a gRPC client."));
                 });
         }
     }
